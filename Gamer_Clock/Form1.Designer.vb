dde151414efa39f6c13f792146eb70a99a1fa3a3
iCAROS7 <hominlab@gmail.com>
2017-11-21 02:43:33 +0900

    init

---
+++ Gamer_Clock/Form1.Designer.vb
@@ -0,0 +1,434 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Hour = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Min = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Sec = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Hours = New System.Windows.Forms.ComboBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Mins = New System.Windows.Forms.ComboBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Secs = New System.Windows.Forms.ComboBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.CopyLeft = New System.Windows.Forms.LinkLabel()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Processs = New System.Windows.Forms.Label()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.Exit_Form = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Open_Form = New System.Windows.Forms.ToolStripMenuItem()
+        Me.App_Name = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GroupBox1.SuspendLayout()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("맑은 고딕", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(116, 30)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "현재 시각 :"
+        '
+        'Hour
+        '
+        Me.Hour.AutoSize = True
+        Me.Hour.Font = New System.Drawing.Font("맑은 고딕", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Hour.Location = New System.Drawing.Point(127, 9)
+        Me.Hour.Name = "Hour"
+        Me.Hour.Size = New System.Drawing.Size(43, 32)
+        Me.Hour.TabIndex = 1
+        Me.Hour.Text = "00"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("맑은 고딕", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Label2.Location = New System.Drawing.Point(162, 20)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(21, 17)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "시"
+        '
+        'Min
+        '
+        Me.Min.AutoSize = True
+        Me.Min.Font = New System.Drawing.Font("맑은 고딕", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Min.Location = New System.Drawing.Point(189, 9)
+        Me.Min.Name = "Min"
+        Me.Min.Size = New System.Drawing.Size(43, 32)
+        Me.Min.TabIndex = 1
+        Me.Min.Text = "00"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("맑은 고딕", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Label4.Location = New System.Drawing.Point(224, 20)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(21, 17)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "분"
+        '
+        'Sec
+        '
+        Me.Sec.AutoSize = True
+        Me.Sec.Font = New System.Drawing.Font("맑은 고딕", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Sec.Location = New System.Drawing.Point(251, 9)
+        Me.Sec.Name = "Sec"
+        Me.Sec.Size = New System.Drawing.Size(43, 32)
+        Me.Sec.TabIndex = 1
+        Me.Sec.Text = "00"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("맑은 고딕", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Label6.Location = New System.Drawing.Point(286, 20)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(21, 17)
+        Me.Label6.TabIndex = 2
+        Me.Label6.Text = "초"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("맑은 고딕", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Label3.Location = New System.Drawing.Point(82, 26)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(21, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "시"
+        '
+        'Hours
+        '
+        Me.Hours.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.Hours.Font = New System.Drawing.Font("맑은 고딕", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Hours.FormattingEnabled = True
+        Me.Hours.ImeMode = System.Windows.Forms.ImeMode.Off
+        Me.Hours.Items.AddRange(New Object() {"00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"})
+        Me.Hours.Location = New System.Drawing.Point(36, 20)
+        Me.Hours.Name = "Hours"
+        Me.Hours.Size = New System.Drawing.Size(46, 28)
+        Me.Hours.Sorted = True
+        Me.Hours.TabIndex = 3
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("맑은 고딕", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Label5.Location = New System.Drawing.Point(155, 26)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(21, 17)
+        Me.Label5.TabIndex = 2
+        Me.Label5.Text = "분"
+        '
+        'Mins
+        '
+        Me.Mins.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.Mins.Font = New System.Drawing.Font("맑은 고딕", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Mins.FormattingEnabled = True
+        Me.Mins.ImeMode = System.Windows.Forms.ImeMode.Off
+        Me.Mins.Location = New System.Drawing.Point(109, 20)
+        Me.Mins.Name = "Mins"
+        Me.Mins.Size = New System.Drawing.Size(46, 28)
+        Me.Mins.TabIndex = 3
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("맑은 고딕", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Label7.Location = New System.Drawing.Point(228, 26)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(21, 17)
+        Me.Label7.TabIndex = 2
+        Me.Label7.Text = "초"
+        '
+        'Secs
+        '
+        Me.Secs.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.Secs.Font = New System.Drawing.Font("맑은 고딕", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Secs.FormattingEnabled = True
+        Me.Secs.ImeMode = System.Windows.Forms.ImeMode.Off
+        Me.Secs.Location = New System.Drawing.Point(182, 20)
+        Me.Secs.Name = "Secs"
+        Me.Secs.Size = New System.Drawing.Size(46, 28)
+        Me.Secs.Sorted = True
+        Me.Secs.TabIndex = 3
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Secs)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Mins)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Hours)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Location = New System.Drawing.Point(14, 124)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(290, 67)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Custom"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(14, 70)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(290, 21)
+        Me.TextBox1.TabIndex = 4
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(199, 447)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(108, 31)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "시작"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(15, 52)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(106, 12)
+        Me.Label8.TabIndex = 5
+        Me.Label8.Text = "OSD 표기 메시지 :"
+        '
+        'CopyLeft
+        '
+        Me.CopyLeft.AutoSize = True
+        Me.CopyLeft.Font = New System.Drawing.Font("맑은 고딕", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.CopyLeft.Location = New System.Drawing.Point(10, 453)
+        Me.CopyLeft.Name = "CopyLeft"
+        Me.CopyLeft.Size = New System.Drawing.Size(135, 17)
+        Me.CopyLeft.TabIndex = 6
+        Me.CopyLeft.TabStop = True
+        Me.CopyLeft.Text = "Copyleft ⓒ hominlab"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Enabled = False
+        Me.RadioButton1.Location = New System.Drawing.Point(12, 102)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(63, 16)
+        Me.RadioButton1.TabIndex = 7
+        Me.RadioButton1.Text = "30분 뒤"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Enabled = False
+        Me.RadioButton2.Location = New System.Drawing.Point(87, 102)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(69, 16)
+        Me.RadioButton2.TabIndex = 7
+        Me.RadioButton2.Text = "1시간 뒤"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Enabled = False
+        Me.RadioButton3.Location = New System.Drawing.Point(162, 102)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(69, 16)
+        Me.RadioButton3.TabIndex = 7
+        Me.RadioButton3.Text = "2시간 뒤"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Checked = True
+        Me.RadioButton4.Location = New System.Drawing.Point(237, 102)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(59, 16)
+        Me.RadioButton4.TabIndex = 7
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "커스텀"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(9, 221)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(295, 220)
+        Me.ListBox1.Sorted = True
+        Me.ListBox1.TabIndex = 8
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(10, 206)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(105, 12)
+        Me.Label9.TabIndex = 9
+        Me.Label9.Text = "선택된 프로세스 : "
+        '
+        'Processs
+        '
+        Me.Processs.AutoSize = True
+        Me.Processs.Location = New System.Drawing.Point(112, 206)
+        Me.Processs.Name = "Processs"
+        Me.Processs.Size = New System.Drawing.Size(133, 12)
+        Me.Processs.TabIndex = 10
+        Me.Processs.Text = "종료할 프로세스를 선택"
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.NotifyIcon1.Text = "Gamer Clock"
+        Me.NotifyIcon1.Visible = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.App_Name, Me.Open_Form, Me.ToolStripSeparator1, Me.Exit_Form})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(211, 76)
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(207, 6)
+        '
+        'Exit_Form
+        '
+        Me.Exit_Form.Name = "Exit_Form"
+        Me.Exit_Form.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.E), System.Windows.Forms.Keys)
+        Me.Exit_Form.Size = New System.Drawing.Size(210, 22)
+        Me.Exit_Form.Text = "종료"
+        '
+        'Open_Form
+        '
+        Me.Open_Form.Name = "Open_Form"
+        Me.Open_Form.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.S), System.Windows.Forms.Keys)
+        Me.Open_Form.Size = New System.Drawing.Size(210, 22)
+        Me.Open_Form.Text = "보이기"
+        '
+        'App_Name
+        '
+        Me.App_Name.Enabled = False
+        Me.App_Name.Name = "App_Name"
+        Me.App_Name.Size = New System.Drawing.Size(210, 22)
+        Me.App_Name.Text = "Gamer Clock for RTSS v1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(315, 487)
+        Me.Controls.Add(Me.Processs)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.RadioButton4)
+        Me.Controls.Add(Me.RadioButton3)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.CopyLeft)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Sec)
+        Me.Controls.Add(Me.Min)
+        Me.Controls.Add(Me.Hour)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Gamer Clock v1 for Riva tunner"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Hour As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Min As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Sec As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Hours As ComboBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Mins As ComboBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Secs As ComboBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label8 As Label
+    Friend WithEvents CopyLeft As LinkLabel
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Processs As Label
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents Exit_Form As ToolStripMenuItem
+    Friend WithEvents Open_Form As ToolStripMenuItem
+    Friend WithEvents App_Name As ToolStripMenuItem
+End Class
---
+++ Gamer_Clock/Form1.vb
@@ -0,0 +1,89 @@
+﻿Public Class Form1
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Hour.Text = DateTime.Now.ToString("HH")
+        Min.Text = DateTime.Now.ToString("mm")
+        Sec.Text = DateTime.Now.ToString("ss")
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Hour.Text = DateTime.Now.ToString("HH")
+        Min.Text = DateTime.Now.ToString("mm")
+        Sec.Text = DateTime.Now.ToString("ss")
+        For Each OneProcess As Process In Process.GetProcesses
+            ListBox1.Items.Add(OneProcess.ProcessName + ".exe")
+        Next
+        Dim i As Long
+        For i = 0 To 60 Step 1
+            If i < 10 Then
+                Dim pad As String
+                pad = i.ToString("00")
+                Mins.Items.Add(pad)
+            Else
+                Mins.Items.Add(i)
+            End If
+        Next
+        For i = 0 To 60 Step 1
+            If i < 10 Then
+                Dim pad As String
+                pad = i.ToString("00")
+                Secs.Items.Add(pad)
+            Else
+                Secs.Items.Add(i)
+            End If
+        Next
+        Hours.SelectedItem = DateTime.Now.ToString("HH")
+        Mins.SelectedIndex = 0
+        Secs.SelectedIndex = 0
+        If Not ListBox1.FindString("RTSS.exe") <> -1 Then
+            MsgBox("RivaTuner Statistics Server가 감지되지 않습니다." + vbCrLf + "프로그램을 종료합니다.", vbCritical, "오류")
+            End
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Processs.Text = "종료할 프로세스를 선택" Then
+            MsgBox("종료할 프로세스를 선택해주세요!!", vbCritical, "오류")
+        Else
+
+            If MsgBox("선택한 프로세스 : " + Processs.Text + vbCrLf + "종료 예정 시간 : " + Hours.Text + "시 " + Mins.Text + "분 " + Secs.Text + "초" + vbCrLf + vbCrLf + "맞냐?", vbQuestion + vbYesNo, "확인") = vbYes Then
+                Me.WindowState = FormWindowState.Minimized
+            End If
+
+        End If
+    End Sub
+
+    Private Sub NotifyIcon1_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles NotifyIcon1.DoubleClick
+        ShowInTaskbar = True
+        Me.WindowState = FormWindowState.Normal
+        NotifyIcon1.Visible = False
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles CopyLeft.LinkClicked
+        Shell("explorer http://minnote.net")
+    End Sub
+
+    Private Sub RadioButton4_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton4.CheckedChanged
+        If RadioButton4.Checked = True Then
+            GroupBox1.Enabled = True
+        Else
+            GroupBox1.Enabled = False
+        End If
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        Processs.Text = ListBox1.SelectedItem
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        If Me.WindowState = FormWindowState.Minimized Then
+            NotifyIcon1.Visible = True
+            NotifyIcon1.Icon = SystemIcons.Application
+            NotifyIcon1.BalloonTipIcon = ToolTipIcon.Info
+            NotifyIcon1.BalloonTipTitle = "Verificador corriendo"
+            NotifyIcon1.BalloonTipText = "Verificador corriendo"
+            NotifyIcon1.ShowBalloonTip(50000)
+            ShowInTaskbar = False
+        End If
+    End Sub
+End Class
